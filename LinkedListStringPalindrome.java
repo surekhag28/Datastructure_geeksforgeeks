2d5531fc994a8f263a21ce4150b51507a1a5dd46
Surekha <surekhag28@gmail.com>
2021-02-02 01:37:40 +1100

    Create LinkedListStringPalindrome.java
    linked list of strings forms a palindrome

---
+++ LinkedListStringPalindrome.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    String data;
+    Node next;
+    Node(String key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Palindrome
+{
+    static Node head;
+    public static void main (String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            String a = sc.next();
+            Node head = new Node(a);
+            addToTheLast(head);
+            for(int i = 1; i < n; i++)
+            {
+                String a1 = sc.next();
+                addToTheLast(new Node(a1));
+            }
+            
+            GfG gfg = new GfG();
+            boolean res = gfg.compute(head);
+            if(res == true)
+            System.out.println("True");
+            else
+            System.out.println("False");
+        }
+    }
+    
+    public static void addToTheLast(Node node)
+    {
+        if(head == null)
+        {
+            head = node;
+        }
+        else
+        {
+            Node temp = head;
+            while(temp.next != null)
+                 temp = temp.next;
+                 temp.next = node;
+        }
+    }
+}// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    String data;
+    Node next;
+    Node(String key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+class GfG
+{
+    public static boolean compute(Node node)
+    {
+        String str="";
+        while(node!=null){
+            str=str.concat(node.data);
+            node=node.next;
+        }
+        
+        int j=str.length()-1;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)!=str.charAt(j))
+                return false;
+            else
+                j--;
+        }
+        return true;
+    }
+}
