aedee232cfd5763c3ad1f6cdc7237431392e285a
Surekha <surekhag28@gmail.com>
2021-02-02 01:51:45 +1100

    Create InsertAtMiddle.java
    Insert in Middle of Linked List

---
+++ InsertAtMiddle.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int t = sc.nextInt();
+        
+        while(t-->0){
+            int n = sc.nextInt();   
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            
+            for(int i=0; i<n-1; i++){
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            
+            //display(head);
+            Solution obj = new Solution();
+            obj.insertInMid(head, key);
+            display(head);
+            System.out.println();
+        }
+    }
+    
+    public static void display(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(curr.data+" ");
+            curr = curr.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+Structure of node class is:
+class Node {
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    
+    public Node insertInMid(Node head, int data){
+        //Insert code here, return the head of modified linked list
+        int len=0;
+        Node curr=head;
+        
+        while(curr!=null){
+            len++;
+            curr=curr.next;
+        }
+        int idx=0;
+        if(len%2!=0)
+            idx=len/2;
+        else
+            idx=len/2-1;
+        curr=head;
+        for(int i=0;i<idx;i++){
+            curr=curr.next;
+        }
+        
+        Node node = new Node(data);
+        node.next=curr.next;
+        curr.next=node;
+        
+        return head;
+    }
+}
