59c173d6c07d545e630b5df1c28aff7d94ae41d4
Surekha <surekhag28@gmail.com>
2021-02-02 01:51:11 +1100

    Create MiddleElementInLL.java
    Finding middle element in a linked list.

---
+++ MiddleElementInLL.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            int ans = g.getMiddle(head);
+            System.out.println(ans);
+            //printList(head); 
+            t--;
+        }
+    } 
+} 
+   // } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution
+{
+    int getMiddle(Node head)
+    {
+         // Your code here.
+         int len=0;
+         Node curr=head;
+         while(curr!=null){
+             len++;
+             curr=curr.next;
+         }
+         
+         int idx=len/2;
+         for(int i=0;i<idx;i++){
+             head=head.next;
+         }
+         
+         return head.data;
+    }
+}
