620a841162cce9c90efcce94f2415850036d8593
Surekha <surekhag28@gmail.com>
2021-02-02 12:46:54 +1100

    Create InsertSortedDLL.java
    Insert in Sorted way in a Sorted DLL

---
+++ InsertSortedDLL.java
@@ -0,0 +1,127 @@
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next, prev;
+    
+    Node(int item)
+    {
+        data = item;
+        next = prev = null;
+    }
+}
+
+class Insert_Sorted_DLL
+{
+    public static boolean isChecked(Node head)
+    {
+        int lengthF=0;
+        int lengthB=0;
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+            lengthF++;
+        }
+        
+        while(temp.prev!=null)
+        {
+            temp=temp.prev;
+            lengthB++;
+        }
+        
+        return lengthF==lengthB? true:false;
+    }
+	
+	public static void main (String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    
+	    while(t-- > 0)
+	    {
+	        int n = sc.nextInt();
+	        
+	        Node head=null, tail=null;
+	        
+	        for (int i = 0;i < n;i++)
+	        {
+	            int a = sc.nextInt();
+	            if (head==null) 
+	            {
+	                head = new Node(a);
+	                tail = head;
+	            }
+	            else
+	            {
+	                tail.next=new Node(a);
+	                tail.next.prev=tail;
+	                tail = tail.next;
+	            }
+	        }
+	        
+	        int x = sc.nextInt();
+	        GfG gfg = new GfG();
+	         head =  gfg.sortedInsert(head, x);
+	         
+	        if(isChecked(head))
+	        printList(head);
+	        else
+	        System.out.print("The node has not been inserted correctly. Please fix the prev and next pointers");
+	        System.out.println();
+	    }
+	}
+	
+	public static void printList(Node head)
+	{
+	    Node temp = head;
+	    while(temp != null)
+	    {
+	        System.out.print(temp.data + " ");
+	        temp = temp.next;
+	    }
+	}
+}
+// } Driver Code Ends
+
+
+/*class of the node of the DLL is as
+class Node {
+	int data;
+    Node prev, next;
+    Node(int item)
+    {data = item;
+        next = prev = null;
+    }
+}
+*/
+// function should insert a new node in sorted way in
+// a sorted doubly linked list
+class GfG
+{
+    public static Node sortedInsert(Node head, int x)
+    {
+        Node curr=head;
+        Node node = new Node(x);
+        
+        while(curr.data<x && curr.next!=null){
+            curr=curr.next;
+        }
+        if(curr==head){
+            node.next=head;
+            head.prev=node;
+            head=node;
+        }else if(curr.next==null){
+            curr.next=node;
+            node.prev=curr;
+        }else{
+            //System.out.println(curr.data);
+            Node prev=curr.prev;
+            prev.next=node;
+            node.next=curr;
+            curr.prev=node;
+            node.prev=prev; 
+        }
+        return head;
+    }
+}
